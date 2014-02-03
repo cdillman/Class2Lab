96d355d8ba30d757c351dc8277075df8c5989e80
Corbin <Corbin@Corbin-THINK>
2014-02-03 06:00:06 -0600

    Added to Lab 4 V1.1

--- src/java/model/WelcomeService.java
+++ src/java/model/WelcomeService.java
@@ -15,52 +15,79 @@ import java.util.*;
  */
 public class WelcomeService {
     private Calendar current = Calendar.getInstance();
+    private Calendar current2 = Calendar.getInstance();
     private Date date = current.getTime();
-    private String mor = "2/1/14 12:00 AM";
-    private String aft = "2/1/14 11:00 AM";
-    private String eve = "2/1/14 5:00 PM";
+    private String mor = "2/3/2014 00:00 AM";
+    private String aft = "2/3/2014 11:00 AM";
+    private String eve = "2/3/2014 5:00 PM";
     private Date after;
     private Date morn;
     private Date even;
-    private String format = "M/d/yy hh:mm a";
+    private String format = "MM/dd/yyyy hh:mm a";
     private SimpleDateFormat sdf = new SimpleDateFormat(format);
     private String dayMsg;
+    private String welMsg = "Good ";
     private String name;
-    private String morning;
-    private String afternoon;
-    private String evning;
+    private String morMsg;
+    private String aftMsg;
+    private String eveMsg;
     
+    public String Run(){
+        WelcomeService wel = new WelcomeService();
+        wel.TimeOfDay();
+        return wel.GreetingMessage();
+    }
     
-    public String TimeOfDay() throws ParseException{
+    public String TimeOfDay(){
+       try{
         sdf.format(date);
         Date morn = sdf.parse(mor);
         Date after = sdf.parse(aft);
         Date even = sdf.parse(eve);
-        
-        if(date.after(morn) && date.before(after) ){
-            System.out.println("Morning");
+       }
+       catch(ParseException pe){
+           
+       }
+        if(date.after(morn) && date.before(after)){
+//            System.out.println("Morning");
+            dayMsg = "morning";
         }
         else if(date.after(after) && date.before(even)){
-            System.out.println("Afternoon");
+//            System.out.println("Afternoon");
+            dayMsg = "afternoon";
         }
         else if(date.after(even) && date.before(morn)){
-            System.out.println("Night");
+//            System.out.println("Night");
+            dayMsg = "evening";
         }
         else{
-            System.out.println("Fail");
+//            System.out.println("Fail");
         }
             return dayMsg;
+    }
+    
+    public String GreetingMessage(){
+        name = "Mike";
+        
+        welMsg += dayMsg + " " + name + ", Welcome";
+        return welMsg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
     
    
     public static void main(String[] args) {
         WelcomeService wel = new WelcomeService();
         
-        try{
+       
         wel.TimeOfDay();
-        }
-        catch(ParseException pe){
-            System.out.println("P.E.");
-        }
+        System.out.println(wel.GreetingMessage());
+        
     }
 }
